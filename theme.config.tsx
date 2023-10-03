f90e327453170cce1f299a1b6939de32c4621b82
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-10-03 21:09:25 +0700

    initial configurations

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { DocsThemeConfig } from "nextra-theme-docs"
+
+const config: DocsThemeConfig = {
+  logo: <span>Tifan's Personal Docs</span>,
+  project: {
+    link: "https://github.com/tifandotme/docs",
+  },
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  footer: {
+    text: "2023",
+  },
+}
+
+export default config
