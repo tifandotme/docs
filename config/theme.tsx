c0e20e817d7d9a39099c0e23b7f3db652c078850
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-10-07 15:28:17 +0700

    chore: edit config

--- config/theme.tsx
+++ config/theme.tsx
@@ -1,7 +1,6 @@
 import { usePathname } from "next/navigation"
 import { DocsThemeConfig } from "nextra-theme-docs"
 import site from "config/site"
-import { Docs } from "components/icons"
 
 const config: DocsThemeConfig = {
   logo: (
@@ -11,18 +10,12 @@ const config: DocsThemeConfig = {
         fontSize: "1.2rem",
       }}
     >
-      Tifan's Personal Docs
+      {site.name}
     </span>
   ),
   useNextSeoProps() {
-    if (usePathname() !== "/") {
-      return {
-        titleTemplate: "%s - Tifan's Personal Docs",
-      }
-    } else {
-      return {
-        title: "Tifan's Docs",
-      }
+    return {
+      titleTemplate: "%s - " + site.name,
     }
   },
   head: () => {
@@ -30,14 +23,13 @@ const config: DocsThemeConfig = {
 
     return (
       <>
-        <link href="/dist/main.css" rel="stylesheet" />
         <meta property="og:url" content={url} />
         <meta property="og:title" content={site.name} />
         <meta property="og:description" content={site.description} />
       </>
     )
   },
-  docsRepositoryBase: "https://github.com/tifandotme/docs",
+  // docsRepositoryBase: "https://github.com/tifandotme/docs",
 }
 
 export default config
