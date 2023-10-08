8ac359bb97d5296a9f30b903428f0e8a6ddceed7
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-10-08 12:33:38 +0700

    update packages, button adjustments, etc

--- components/icons.tsx
+++ components/icons.tsx
@@ -3,15 +3,12 @@ export function Docs() {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      aria-hidden="true"
-      strokeWidth="2"
       stroke="currentColor"
+      fill="currentColor"
+      stroke-width="0"
     >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
-      ></path>
+      <path d="M19.875 3H4.125C2.953 3 2 3.897 2 5v14c0 1.103.953 2 2.125 2h15.75C21.047 21 22 20.103 22 19V5c0-1.103-.953-2-2.125-2zm0 16H4.125c-.057 0-.096-.016-.113-.016-.007 0-.011.002-.012.008L3.988 5.046c.007-.01.052-.046.137-.046h15.75c.079.001.122.028.125.008l.012 13.946c-.007.01-.052.046-.137.046z"></path>
+      <path d="M6 7h6v6H6zm7 8H6v2h12v-2h-4zm1-4h4v2h-4zm0-4h4v2h-4z"></path>
     </svg>
   )
 }
@@ -20,11 +17,10 @@ export function External() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
       stroke="currentColor"
       fill="currentColor"
       stroke-width="0"
-      viewBox="0 0 20 20"
-      aria-hidden="true"
     >
       <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path>
       <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path>
